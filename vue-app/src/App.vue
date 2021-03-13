c5f79ca2069115a19c13aab94c4b38a6aafdd5ca
benoit <benoit.zenker@gmail.com>
2021-03-13 18:09:19 +0100

    creation dossier vue

---
+++ vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Scene3D/>
+  </div>
+</template>
+
+<script>
+
+import Scene3D from "./components/Scene3D.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Scene3D,
+  }
+}
+</script>
+
+<style>
+#app {
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ vue-app/src/components/Scene3D.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Scene3D">
+      <p>gytytty</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Scene3D',
+
+  props: {
+    msg: String
+  },
+
+  mounted:function(){
+    console.log("Scene3D mounted");
+  }
+
+}
+
+</script>
+
+
+<style scoped>
+</style>
